3b7ffc17f29e3353897d3db102b0163dd4ac05c3
STRnick <STRnick.nicksumin97@naver.com>
2022-08-02 15:49:26 +0900

    sumin branch update 220802

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <h1>
-        수정해봤습니다.
-      </h1>
+      <div>
+          <h1>변경해보았습니다.</h1>
+      </div>
     </div>
+
   );
 }
