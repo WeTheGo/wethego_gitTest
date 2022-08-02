1d3c3f881ea66c70c15397ee91494a0e54f5fac4
STRnick <STRnick.nicksumin97@naver.com>
2022-08-02 18:21:51 +0900

    bootstrap test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,18 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import AdminPage from "./pages/AdminPage";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/*<Header />*/}
       <Navigation />
       <Routes>
-        <Route path="/" element={<Sumin />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/admin" element={<AdminPage/>} />
+          <Route path="/" element={<Sumin />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminPage/>} />
+          <Route path="/test" element={<Dashboard/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
